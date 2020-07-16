916571c054b0ec718451192eeb646541e2fcf821
moein rahimi <imrrahimi@gmail.com>
2020-07-16 13:51:33 +0430

    version 0.1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow,ipcMain  } = require('electron')
 const artstation = require('./artstation')
+const wallpaper = require('./helpers/wallpaper')
 function createWindow () {
   // Create the browser window.
   const win = new BrowserWindow({
@@ -10,14 +11,21 @@ function createWindow () {
     }
   })
 
-  // and load the index.html of the app.
   win.loadFile('index.html')
 }
 
 app.whenReady().then(createWindow)
 
 ipcMain.on('search', async (event, q) => {
-  // console.log(q) // prints "ping"
-  let res = await artstation.run(q)
+  let res = await artstation.search(q)
   event.reply('search_result', res)
 })
+ipcMain.on('getArtwork', async (event, artworkPage) => {
+  let res = await artstation.getArtwork(artworkPage)
+  wallpaper.startWallpaperSlider(res)
+  event.reply('artwork_result', res)
+})
+
+
+
+
--- artstation.js
+++ artstation.js
@@ -1,59 +1,68 @@
 const puppeteer = require('puppeteer');
-const axios = require('axios')
-const fs = require('fs')
-exports.run = async (q) => {
-  console.log("exports.run -> q", q)
-  try{
-  const browser = await puppeteer.launch({headless:false});
-  const page = await browser.newPage();
-  await page.setRequestInterception(true);
-  // we don't want to pressure on our bandwidth do we ? :)
-  page.on('request', request => {
-    if (request.resourceType() === 'image')
-      request.abort();
-    else
-      request.continue();
-  });
 
-  let baseURL = `https://www.artstation.com/search?q=${encodeURI(q)}&sort_by=relevance`
+exports.search = async (q) => {
+  console.log(q, 'query');
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setUserAgent(
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+    );
+    await page.setRequestInterception(true);
+    // we don't want to pressure on our bandwidth do we ? :)
+    page.on('request', (request) => {
+      if (request.resourceType() === 'image') request.abort();
+      else request.continue();
+    });
 
+    let baseURL = `https://www.artstation.com/search?q=${encodeURI(q)}&sort_by=relevance`;
     await page.goto(baseURL);
     await page.setDefaultNavigationTimeout(0);
-  // await page.click('form [name="q"]')
-  // await page.keyboard.type(searching)
-  // await page.click('form [type="submit"]')
+    let items = await page.evaluate(() => {
+      let arr = [];
+      document.querySelectorAll('.gallery-grid-item').forEach((a) => {
+        arr.push({
+          thumbnail: a.children[0].children[2].src,
+          artwork: a.children[0].href,
+        });
+      });
+      return arr;
+    });
+    browser.close();
+    return items;
+  } catch (e) {
+    console.log(e, 'got an error !');
+  }
+};
+
+exports.getArtwork = async (url) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setUserAgent(
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+    );
+    await page.setRequestInterception(true);
+    page.on('request', (request) => {
+      if (request.resourceType() === 'image') request.abort();
+      else request.continue();
+    });
+
+    await page.goto(url);
+    await page.setDefaultNavigationTimeout(0);
 
     let items = await page.evaluate(() => {
-    let arr = []
-    document.querySelectorAll(".gallery-grid-item").forEach(a => arr.push(a.children[0].children[2].src))
-    // document.querySelectorAll(".gallery-grid-item").forEach(a => arr.push(a.children[0].href))
-    return arr
+      let artworks = [];
+      document.querySelectorAll('.asset-actions').forEach((dlBtn) => {
+        artworks.push(dlBtn.children[0].href);
+      });
 
-  })
+      return artworks;
+    });
+    browser.close();
 
-    console.log(items, 'items')
-    return items
-    // await page.goto(wallpaperPage);
-    // let wallpapersLength = await page.evaluate(()=>{
-    //   return  document.querySelectorAll('#single >  div > .wrap-image').length
-    // })
-    // console.log(wallpapersLength,'length')
-    // let links = []
-    // for(var i = 2;i<wallpapersLength;i++){
-    //   let linkSelector = `#single > div:nth-child(${i}) > .wrap-image > a`
-    //   const link = await page.evaluate((sel)=>{
-    //     return  document.querySelector(sel).href.replace('/w/','/download/')
-    //   },linkSelector)
-    //   let {data} =await axios.get(link, {
-    //     responseType: 'arraybuffer'
-    // })
-    //   let randomnumber = Math.floor(Math.random() * (1555 - 51 + 1)) + 51;
-    //   fs.writeFileSync(`./images/${randomnumber}.jpg`,data)
-    //   console.log(data)
-    // }
-    // await broweser.close()
-  }catch(e){
-    console.log(e,'got an error !')
+    return items;
+  } catch (e) {
+    console.log(e, 'got an error !');
   }
-}
-// run()
+};
